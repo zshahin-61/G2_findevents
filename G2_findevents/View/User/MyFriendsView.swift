a85f2ed88130f6162da89b516447c3dd32f9c82a
zshahin-61 <zshahin.61@gmail.com>
2023-07-10 17:37:56 -0400

    update
    z

--- G2_findevents/View/User/MyFriendsView.swift
+++ G2_findevents/View/User/MyFriendsView.swift
@@ -27,18 +27,37 @@ struct MyFriendsView: View {
                     Text("No Friends exist to Show")
                 }
                 
-                ForEach(dbHelper.myFriendsList) { myFrnd in
-                    VStack {
-                        Text(myFrnd.name)
-                        Text(myFrnd.contactNumber)
+                ForEach(dbHelper.myFriendsList, id:\.id) { myFrnd in
+                    HStack {
+                        VStack(alignment: .leading) {
+                                                Text(myFrnd.name)
+                                                    .font(.headline)
+                                                Text(myFrnd.contactNumber)
+                                                    .font(.subheadline)
+                                            }
                         Spacer()
                         if let imageData = myFrnd.image {
                             Image(uiImage: UIImage(data: imageData)!)
                                 .resizable()
                                 .frame(width: 100, height: 100)
                         }
+                      
                     }
-                }
+                }.onDelete(perform: { indexSet in
+                    for index in indexSet{
+                        //get the  object to delete
+                        let friend = self.dbHelper.myFriendsList[index]
+                        //delete the document from database
+                        
+                        if let friendID = friend.id {
+                                   self.dbHelper.deleteMyFriend(friendID: friendID)
+                                   print("friendID: \(friendID)")
+                               } else {
+                                   print("Friend ID is nil")
+                               }
+                    }
+                    
+                })
             }
             Spacer()
         }
