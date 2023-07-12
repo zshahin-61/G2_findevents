5dec7abc5496bc9a946876bf8cd5adeed2237a35
GolnazCh <golnaz.chehrazi@gmail.com>
2023-07-12 18:43:42 -0400

    Update

--- G2_findevents/Views/app/SignInView.swift
+++ G2_findevents/Views/app/SignInView.swift
@@ -7,30 +7,29 @@
 
 import SwiftUI
 
-
 struct SignInView: View {
     @EnvironmentObject var authHelper : FireAuthController
     @EnvironmentObject var dbHelper: FirestoreController
-
-    @State private var email : String = "g.chehrazi@gmail.com"
-    @State private var password : String = "Admin123"
-
+    
+    @State private var emailFromUI : String = "g.chehrazi@gmail.com"
+    @State private var passwordFromUI : String = "Admin123"
+    
     @Binding var rootScreen : RootView
     
     private let gridItems : [GridItem] = [GridItem(.flexible()), GridItem(.flexible())]
     
-    //private var currentUser : UserProfile?
+    @State private var showAlert = false
     
     var body: some View {
         
         VStack{
-
+            
             Form{
-                TextField("Enter Email", text: self.$email)
+                TextField("Enter Email", text: self.$emailFromUI)
                     .textInputAutocapitalization(.never)
                     .textFieldStyle(.roundedBorder)
                 
-                SecureField("Enter Password", text: self.$password)
+                SecureField("Enter Password", text: self.$passwordFromUI)
                     .textInputAutocapitalization(.never)
                     .textFieldStyle(.roundedBorder)
             }
@@ -38,8 +37,7 @@ struct SignInView: View {
             
             LazyVGrid(columns: self.gridItems){
                 Button(action: {
-                  
-                    self.authHelper.signIn(email: self.email, password: self.password, withCompletion: { isSuccessful in
+                    self.authHelper.signIn(email: self.emailFromUI, password: self.passwordFromUI, withCompletion: { isSuccessful in
                         if (isSuccessful){
                             self.dbHelper.getUserProfile(withCompletion: {isSuccessful in
                                 dbHelper.getFriends()
@@ -48,6 +46,7 @@ struct SignInView: View {
                             self.rootScreen = .Home
                         }else{
                             //show the alert with invalid username/password prompt
+                            showAlert = true
                             print(#function, "invalid username/password")
                         }
                     })
@@ -58,15 +57,19 @@ struct SignInView: View {
                         .foregroundColor(.white)
                         .bold()
                         .padding()
-                        .background(Color.blue)
+                    
+                    
                 }
+                //.background(Color.blue)
+                .disabled(self.emailFromUI.isEmpty || self.passwordFromUI.isEmpty || !isEmailValid() )
+                .buttonStyle(CustomButtonStyle(isEnabled: !self.emailFromUI.isEmpty && !self.passwordFromUI.isEmpty && isEmailValid()))
+                
                 
                 Button(action: {
                     self.rootScreen = .SignUp
-
-
-                }){
                     
+                    
+                }){
                     Text("Sign Up")
                         .font(.title2)
                         .foregroundColor(.white)
@@ -75,10 +78,36 @@ struct SignInView: View {
                         .background(Color.blue)
                 }
             }
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Alert Title"),
+                    message: Text("invalid username/password"),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
         }
-
-        
     }
+    
+    // MARK: func for check if the form is valid
+    func isEmailValid()-> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: emailFromUI)
+    }
+    
+    struct CustomButtonStyle: ButtonStyle {
+        let isEnabled: Bool
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                //.padding()
+                .foregroundColor(.white)
+                .background(!self.isEnabled ? Color.gray : Color.blue)
+                .cornerRadius(8)
+                .opacity(!self.isEnabled ? 0.8 : 1.0)
+                //.disabled(!self.isEnabled)
+        }
+    }
+    
 }
 
 
